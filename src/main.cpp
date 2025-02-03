aab8ec8219ace7510b3084c9d37b744ab161d1b9
doddy-s <doddy.s@protonmail.com>
2025-02-03 09:50:28 +0700

    checkpoint

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 #include "../libs/http/httplib.h"
 #include "../libs/nlohmann/json.hpp"
-#include <sqlite3.h>
-#include "../libs/xxhash/xxhash.h"
 #include <torch/script.h>
 #include <libstemmer.h>
+#include <filesystem>
+#include <chrono>
+#include "../libs/xxhash/xxhash.h"
+#include "../libs/cxxopts/cxxopts.hpp"
 
 #define author "doddy-s"
 #define version "v0.1"
 #define appName "BlockTheTweet Inference"
 
-torch::jit::script::Module MODEL;
-nlohmann::json_abi_v3_11_3::json WORD_INDEX;
-sb_stemmer* STEMMER;
+// Global variables
+torch::jit::script::Module MODEL; // Single model
+nlohmann::json WORD_INDEX; // Word index for tokenization
+sb_stemmer* STEMMER; // Stemmer for preprocessing text
 
-// BEGIN OF STRUCTS
+// Struct to hold prediction results
 struct Prediction {
-    int id;
-    std::string timestamp;
-    uint64_t text_hash;
     std::string text;
+    uint64_t text_hash;
     float confidence;
     long long nanosecond;
 
@@ -32,124 +33,29 @@ struct Prediction {
         }.dump();
     }
 };
-// END OF STRUCTS
 
-// BEGIN OF DATABASE
-class Database {
-private:
-    sqlite3* db;
-
-    bool executeSql(const std::string sql) {
-        char* errorMessage;
-        int rc = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &errorMessage);
-        if (rc != SQLITE_OK) {
-            std::cerr << "SQL error: " << errorMessage << std::endl;
-            sqlite3_free(errorMessage);
-            return false;
-        }
-        return true;
-    }
-
-public:
-    Database(const std::string& dbPath) {
-        // Open database
-        int rc = sqlite3_open(dbPath.c_str(), &db);
-        if (rc) {
-            throw std::runtime_error("Cannot open database: " + std::string(sqlite3_errmsg(db)));
-        }
-
-        // Create errors table if not exists
-        std::string createTableSQL = R"(
-            CREATE TABLE IF NOT EXISTS logs (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-                log_type TEXT,
-                message TEXT
-            );
-            
-            CREATE TABLE IF NOT EXISTS predictions (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-                text_hash INTEGER UNIQUE,
-                text TEXT,
-                confidence FLOAT,
-                second INTEGER
-            );
-        )";
-
-        if (!executeSql(createTableSQL)) {
-            throw std::runtime_error("Failed to create tables");
-        }
-    }
-
-    // bool writePrediction(const Prediction& pred) {
-    //     // Create the SQL statement to insert data into the predictions table
-    //     std::string sql = R"(
-    //         INSERT INTO predictions (text_hash, text, confidence, second)
-    //         VALUES (?, ?, ?, ?);
-    //     )";
-
-    //     // Prepare the SQL statement
-    //     sqlite3_stmt* stmt;
-    //     int rc = sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr);
-    //     if (rc != SQLITE_OK) {
-    //         std::cerr << "Failed to prepare statement: " << sqlite3_errmsg(db) << std::endl;
-    //         return false;
-    //     }
-
-    //     // Bind the values from the Prediction struct to the prepared statement
-    //     sqlite3_bind_int(stmt, 1, pred.text_hash); // Bind text_hash
-    //     sqlite3_bind_text(stmt, 2, pred.text.c_str(), -1, SQLITE_STATIC);
-    //     sqlite3_bind_double(stmt, 3, pred.confidence);
-    //     sqlite3_bind_double(stmt, 4, pred.nanosecond);
-
-    //     // Execute the SQL statement
-    //     rc = sqlite3_step(stmt);
-    //     if (rc != SQLITE_DONE) {
-    //         std::cerr << "Execution failed: " << sqlite3_errmsg(db) << std::endl;
-    //         sqlite3_finalize(stmt);
-    //         return false;
-    //     }
-
-    //     // Finalize the statement to release resources
-    //     sqlite3_finalize(stmt);
-    //     return true;
-    // }
-
-};
-Database* DATABASE;
-// END OF DATABASE
-
-// BEGIN OF UTILS
-std::string constructResponse(const int& statusCode, const std::string& message) {
+// Utility function to construct a JSON response
+std::string constructResponse(const int& statusCode, const std::string& message, nlohmann::json data = nullptr) {
     auto response = nlohmann::json{
         {"statusCode", statusCode},
-        {"message", message} };
+        {"message", message}
+    };
 
-    return response.dump();
-}
-
-std::string constructResponse(const int& statusCode, const std::string& message, nlohmann::json& data) {
-    auto response = nlohmann::json{
-        {"statusCode", statusCode},
-        {"message", message} };
-
-    if (data != NULL) {
+    if (data != nullptr) {
         response["data"] = data;
     }
 
     return response.dump();
 }
 
+// Function to stem a word using the stemmer
 std::string stemWord(const std::string& word) {
     const sb_symbol* stemmed = sb_stemmer_stem(STEMMER, (const sb_symbol*)word.c_str(), word.size());
-
     int stemmed_length = sb_stemmer_length(STEMMER);
-
     return std::string(reinterpret_cast<const char*>(stemmed), stemmed_length);
 }
 
-
+// Function to tokenize text
 std::vector<int64_t> tokenizeText(const std::string& text, size_t max_length) {
     std::vector<int64_t> tokenized_text;
 
@@ -158,9 +64,10 @@ std::vector<int64_t> tokenizeText(const std::string& text, size_t max_length) {
     std::string word;
 
     while (stream >> word) {
-        // Convert word to lowercase to ensure case-insensitivity (optional)
+        // Convert word to lowercase to ensure case-insensitivity
         for (auto& c : word) c = tolower(c);
 
+        // Stem the word
         word = stemWord(word);
 
         // Check if the word exists in the word_index, otherwise assign default value (e.g., 0)
@@ -172,50 +79,47 @@ std::vector<int64_t> tokenizeText(const std::string& text, size_t max_length) {
         }
     }
 
-    // If the vector is longer than max_length, truncate it
+    // Truncate or pad the vector to match max_length
     if (tokenized_text.size() > max_length) {
         tokenized_text.resize(max_length);
     }
-    // If the vector is shorter than max_length, pad it with 0s
     else if (tokenized_text.size() < max_length) {
         tokenized_text.resize(max_length, 0);  // Pad with 0
     }
 
-    for (size_t i = 0; i < 10 && i < tokenized_text.size(); ++i) {
-        std::cout << tokenized_text[i] << " ";
-    }
-    std::cout << std::endl;
-
     return tokenized_text;
 }
 
+// Function to predict text using the loaded model
 bool predictText(const std::string& text, Prediction& prediction) {
     try {
-        std::vector<int64_t> input_data = tokenizeText(text, 295);
+        // Tokenize the input text
+        std::vector<int64_t> input_data = tokenizeText(text, 32);
         torch::Tensor input_tensor = torch::tensor(input_data, torch::dtype(torch::kLong)).unsqueeze(0);
         std::vector<torch::jit::IValue> inputs;
         inputs.push_back(input_tensor);
 
+        // Perform prediction and measure time
         auto beginOfPredictTime = std::chrono::high_resolution_clock::now();
         at::Tensor output = MODEL.forward(inputs).toTensor();
         auto endOfPredictTime = std::chrono::high_resolution_clock::now();
         auto predictTime = std::chrono::duration_cast<std::chrono::nanoseconds>(endOfPredictTime - beginOfPredictTime).count();
 
-        float confidence = output.item<float>();
-
+        // Store prediction results
         prediction.text = text;
-        prediction.confidence = confidence;
+        prediction.text_hash = XXH64(text.c_str(), text.size(), 0);
+        prediction.confidence = output.item<float>();
         prediction.nanosecond = predictTime;
 
         return true;
     }
     catch (const std::exception& e) {
+        std::cerr << "Prediction error: " << e.what() << std::endl;
         return false;
     }
 }
-// END OF UTILS
 
-// BEGIN OF CONTROLLER
+// Controller for handling text classification requests
 void postClassifyText(const httplib::Request& req, httplib::Response& res) {
     res.set_header("Access-Control-Allow-Origin", "*");
     try {
@@ -233,11 +137,11 @@ void postClassifyText(const httplib::Request& req, httplib::Response& res) {
 
         Prediction prediction;
 
-        auto text_hash = XXH64(text.c_str(), text.size(), 0);
-        prediction.text_hash = text_hash;
-
-        predictText(text, prediction);
-        // DATABASE->writePrediction(prediction);
+        if (!predictText(text, prediction)) {
+            res.status = 500;
+            res.set_content(constructResponse(500, "Internal Server Error"), "application/json");
+            return;
+        }
 
         res.status = 200;
         res.set_content(prediction.toResponseData(), "application/json");
@@ -250,15 +154,19 @@ void postClassifyText(const httplib::Request& req, httplib::Response& res) {
     }
 }
 
+// Controller for providing application information
 void getInformations(const httplib::Request&, httplib::Response& res) {
     res.set_header("Access-Control-Allow-Origin", "*");
-    auto data = nlohmann::json{ {"author", author}, {"version", version}, {"appName", appName} };
+    auto data = nlohmann::json{
+        {"author", author},
+        {"version", version},
+        {"appName", appName}
+    };
     res.status = 200;
     res.set_content(constructResponse(200, "success", data), "application/json");
 }
-// END OF CONTROLLER
 
-// BEGIN OF ROUTES
+// Function to attach routes to the server
 void attachRoutes(httplib::Server& server) {
     server.Options(".*", [](const httplib::Request&, httplib::Response& res) {
         res.set_header("Access-Control-Allow-Origin", "*");
@@ -271,21 +179,44 @@ void attachRoutes(httplib::Server& server) {
     server.Get("/", getInformations);
     server.Post("/", postClassifyText);
 }
-// END OF ROUTES
 
-// BEGIN OF MAIN
-int main() {
+// Main function
+int main(int argc, char* argv[]) {
+    // Parse command-line arguments
+    cxxopts::Options options("BlockTheTweet", "A server for text classification using PyTorch models.");
+    options.add_options()
+        ("m,model-path", "Path to the model file", cxxopts::value<std::string>()->default_value("./resources/model.pt"))
+        ("w,word-index-path", "Path to word index JSON file", cxxopts::value<std::string>()->default_value("./resources/word_index.json"))
+        ("s,stemmer-lang", "Stemmer language", cxxopts::value<std::string>()->default_value("english"))
+        ("p,port", "Port to run the server on", cxxopts::value<int>()->default_value("3000"))
+        ("h,help", "Print usage");
+
+    auto result = options.parse(argc, argv);
+
+    if (result.count("help")) {
+        std::cout << options.help() << std::endl;
+        return 0;
+    }
+
+    std::string modelPath = result["model-path"].as<std::string>();
+    std::string wordIndexPath = result["word-index-path"].as<std::string>();
+    std::string stemmerLang = result["stemmer-lang"].as<std::string>();
+    int port = result["port"].as<int>();
+
     httplib::Server server;
 
     try {
-        MODEL = torch::jit::load("./bilstm-en-683k.pt");
+        // Load the model
+        MODEL = torch::jit::load(modelPath);
+        std::cout << "Loaded model from: " << modelPath << std::endl;
 
-        std::ifstream f("./word-index.json");
+        // Load word index for tokenization
+        std::ifstream f(wordIndexPath);
         WORD_INDEX = nlohmann::json::parse(f);
+        std::cout << "Loaded word index from: " << wordIndexPath << std::endl;
 
-        DATABASE = new Database("./block_the_tweet.sqlite");
-
-        STEMMER = sb_stemmer_new("english", nullptr);
+        // Initialize the stemmer
+        STEMMER = sb_stemmer_new(stemmerLang.c_str(), nullptr);
     }
     catch (const c10::Error& e) {
         std::cerr << "Error loading the model: " << e.what() << std::endl;
@@ -296,9 +227,10 @@ int main() {
         return -1;
     }
 
+    // Attach routes to the server
     attachRoutes(server);
 
-    std::cout << "BlockTheTweet Server Is Running At Port 3000\n";
-    server.listen("0.0.0.0", 3000);
+    // Start the server
+    std::cout << "BlockTheTweet Server Is Running At Port " << port << "\n";
+    server.listen("0.0.0.0", port);
 }
-// END OF MAIN
